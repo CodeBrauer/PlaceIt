fed605bc9b995bbfd3a30b56ed845ec6f06c3dab
CodeBrauer <gabrielwanzek@ymail.com>
2014-11-14 23:19:56 +0100

    Added reload button for image to index view

--- templates/index.php
+++ templates/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>PlaceIt</title>
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
     <div class="container">
@@ -13,7 +14,7 @@
         <br>
         <div class="row">
             <div class="col-md-9">
-                <h3>Possible calls:</h3>
+                <h3>Possible calls for colorful placeholders:</h3>
                 <table class="table">
                     <thead>
                         <tr><th>Route</th><th>Respone</th></tr>
@@ -39,11 +40,20 @@
                 </table>
             </div>
             <div class="col-md-3">
-                <img src="300.png" class="img-thumbnail img-responsive">
+                <p class="text-center">
+                    <img src="250.png" class="img-thumbnail img-responsive preview-thumb">
+                    <br><br>
+                    <a class="btn btn-default refresh-btn"><i class="fa fa-refresh"></i></a>
+                </p>
             </div>
         </div>
     </div>
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script>
+        $('.refresh-btn').bind('click', function() {
+            $('.preview-thumb').attr('src', '250.png?tmp=' + (new Date).getTime());
+        });
+    </script>
 </body>
 </html>
